7eb4224d9f22f89f6f0eb24b2142ef46caee7555
Pau Febrer Castro <pfebrerc@gmail.com>
2026-05-05 12:38:33 +0200

    Afegir objectes de conexió a la Base de Dades

--- src/test/java/JavaTesting/Resources/AppParameters.java
+++ src/test/java/JavaTesting/Resources/AppParameters.java
@@ -2,9 +2,37 @@ package JavaTesting.Resources;
 
 public class AppParameters {
 
+    // afegir-hi objectes conexió de Base de Dades
+
     public static final String DOMAIN = "127.0.0.1";
 
     public static final String PORT = "80";
+
+    // public final static Object DD_CON = new ClasseConexioBBDD();
+
+    // public static Object MESSAGES_TRANSLATIONS;
+
+    // Implementar SINGLEON PATTERN
+
+    private static AppParameters instance;
+
+    private AppParameters() {
+
+    }
+    private void initApp(){
+        // codi necessari per la inicialització de l'app
+    }
+
+    public void resetApp(){
+        // codi per reconfigurar l'App
+    }
+
+    public static AppParameters getInstance() {
+        if (instance == null) {
+            instance = new AppParameters();
+        }
+        return instance;
+    }
 
     public static String deployedURL(){
         return "http://" + DOMAIN + ":" + PORT;
