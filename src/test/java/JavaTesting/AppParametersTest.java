216efb5c8eff1649868b9e8311198bd337f1d83f
Pau Febrer Castro <pfebrerc@gmail.com>
2026-05-05 12:36:00 +0200

    creació de Tests a la calse AppParametersTest

---
+++ src/test/java/JavaTesting/AppParametersTest.java
@@ -0,0 +1,31 @@
+package JavaTesting;
+
+import JavaTesting.Resources.AppParameters;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppParametersTest {
+    public static final String DOMAIN_EXPECTED = "127.0.0.1";
+
+    public static final String PORT_EXPECTED = "80";
+
+    public static final String URL_EXPECTED = "http://" + DOMAIN_EXPECTED + ":" + PORT_EXPECTED;
+
+    @Test
+    public void domainURLCorrect(){
+
+        Assert.assertEquals("Retorna una part FQDN", DOMAIN_EXPECTED, AppParameters.DOMAIN);
+    }
+
+    @Test
+    public void correctPort(){
+
+        Assert.assertEquals("Retorna el port correcte", PORT_EXPECTED, AppParameters.PORT);
+    }
+
+    @Test
+    public void correctURL(){
+
+        Assert.assertEquals("Retorna la URL de forma correcte", URL_EXPECTED, AppParameters.deployedURL());
+    }
+}
